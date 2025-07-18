5bd4036752065fcb81c70c5dbfd93ae119657844
jakob.deutloff <jakob.deutloff@gmail.com>
2025-07-18 16:54:41 +0200

    first comparison of CCIC and dardar

--- scripts/plot/ccic.py
+++ scripts/plot/ccic.py
@@ -3,29 +3,32 @@ import ccic
 import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
+import s3fs
 
 # %% load dardar 
 dardar_raw = xr.open_dataset("/work/bm1183/m301049/dardar/dardar_iwp_2008.nc")
-mask = (dardar_raw['latitude'] > -30) & (dardar_raw['latitude'] < 30) 
+mask = (dardar_raw['latitude'] > -20) & (dardar_raw['latitude'] < 20) 
 dardar_raw = dardar_raw.where(mask)
-
-# %%
-ds = xr.open_zarr('/work/bm1183/m301049/ccic/ccic_cpcir_200801010100.zarr')
 
 # %% load ccic
 ds = xr.open_mfdataset("/work/bm1183/m301049/ccic/*.zarr", engine='zarr')
 
 # %% get histograms 
 hists = {}
-iwp_bins = np.logspace(-4, 2, 100)
-hists['dardar'], edges = np.histogram(dardar_raw['iwp']*1e-3, bins=iwp_bins)
-hists['ccic'], edges = np.histogram(ds['tiwp'].sel(latitude=slice(30, -30)), bins=iwp_bins)
+iwp_bins = np.logspace(-4, np.log10(40), 100)
+hists['dardar'], edges = np.histogram(dardar_raw['iwp']*1e-3, bins=iwp_bins, density=False)
+hists['dardar'] = hists['dardar'] / dardar_raw['iwp'].count().values  # Normalize histogram
+hists['ccic'], edges = np.histogram(ds['tiwp'].sel(latitude=slice(20, -20)), bins=iwp_bins, density=False)
+hists['ccic'] = hists['ccic'] /  ds['tiwp'].sel(latitude=slice(20, -20)).size
 # %%
 fig, ax = plt.subplots(figsize=(10, 5))
 
-ax.stairs(hists['ccic'], edges, color='red', label='CCIC')
-ax.stairs(hists['dardar'], edges, color='blue', label='DARDAR')
+ax.stairs(hists['ccic'], edges, color='red', label='CCIC CPCIR')
+ax.stairs(hists['dardar'], edges, color='blue', label='DARDAR v2.1')
 ax.set_xlabel('Ice Water Path (kg m$^{-2}$)')
 ax.set_ylabel('Counts')
 ax.set_xscale('log')
+ax.spines[['top', 'right']].set_visible(False)
+ax.legend()
+fig.savefig('plots/ccic_histogram.png', bbox_inches='tight', dpi=300)
 # %%
