5594a21a67d4d9c2b8adb56d707c7e188d94329b
jakob.deutloff <jakob.deutloff@gmail.com>
2025-08-28 17:22:25 +0200

    Right bins used for hist

--- scripts/plot/ccic.py
+++ scripts/plot/ccic.py
@@ -5,30 +5,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 import s3fs
 
-# %% load dardar 
+# %% load dardar
 dardar_raw = xr.open_dataset("/work/bm1183/m301049/dardar/dardar_iwp_2008.nc")
-mask = (dardar_raw['latitude'] > -20) & (dardar_raw['latitude'] < 20) 
+mask = (dardar_raw["latitude"] > -20) & (dardar_raw["latitude"] < 20)
 dardar_raw = dardar_raw.where(mask)
 
 # %% load ccic
-ds = xr.open_mfdataset("/work/bm1183/m301049/ccic/*.zarr", engine='zarr')
+ds = xr.open_mfdataset("/work/bm1183/m301049/ccic/*.zarr", engine="zarr")
+ds = ds["tiwp"].sel(latitude=slice(20, -20)).load()
 
-# %% get histograms 
+
+# %% get histograms
 hists = {}
-iwp_bins = np.logspace(-4, np.log10(40), 100)
-hists['dardar'], edges = np.histogram(dardar_raw['iwp']*1e-3, bins=iwp_bins, density=False)
-hists['dardar'] = hists['dardar'] / dardar_raw['iwp'].count().values  # Normalize histogram
-hists['ccic'], edges = np.histogram(ds['tiwp'].sel(latitude=slice(20, -20)), bins=iwp_bins, density=False)
-hists['ccic'] = hists['ccic'] /  ds['tiwp'].sel(latitude=slice(20, -20)).size
+bins = np.logspace(-3, 2, 254)[::4]
+hists["dardar"], edges = np.histogram(
+    dardar_raw["iwp"] * 1e-3, bins=bins, density=False
+)
+hists["dardar"] = (
+    hists["dardar"] / dardar_raw["iwp"].count().values
+)  # Normalize histogram
+hists["ccic"], edges = np.histogram(
+    ds.values, bins=bins, density=False
+)
+hists["ccic"] = hists["ccic"] / ds.size
 # %%
 fig, ax = plt.subplots(figsize=(10, 5))
 
-ax.stairs(hists['ccic'], edges, color='red', label='CCIC CPCIR')
-ax.stairs(hists['dardar'], edges, color='blue', label='DARDAR v2.1')
-ax.set_xlabel('Ice Water Path (kg m$^{-2}$)')
-ax.set_ylabel('Counts')
-ax.set_xscale('log')
-ax.spines[['top', 'right']].set_visible(False)
+ax.stairs(hists["ccic"], edges, color="red", label="CCIC CPCIR")
+ax.stairs(hists["dardar"], edges, color="blue", label="DARDAR v2.1")
+ax.set_xlabel("$I$ / kg m$^{-2}$")
+ax.set_ylabel("$P(I)$")
+ax.set_xscale("log")
+ax.spines[["top", "right"]].set_visible(False)
 ax.legend()
-fig.savefig('plots/ccic_histogram.png', bbox_inches='tight', dpi=300)
+fig.savefig("plots/ccic_histogram.png", bbox_inches="tight", dpi=300)
 # %%
